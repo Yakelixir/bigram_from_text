c556fe74ae97c8f1338fa67199032f6b63a5ceea
yakelixir <yakelixir@gmail.com>
2019-06-02 21:21:59 -0500

    parse considerations added for LTR Markers and docs updated / clean up

--- bgparse.py
+++ bgparse.py
@@ -11,6 +11,7 @@ def nltk_parse(data):
 
     """
     Parsing with a pure nltk approach
+    :str required:
     """
 
     from nltk import word_tokenize, bigrams, FreqDist
@@ -28,6 +29,7 @@ def bigram_parse(data):
 
     """
     Parse strings into bigrams without nltk
+    :str required:
     """
 
     from collections import Counter
@@ -48,13 +50,33 @@ def filter_punkt(raw_data):
     filter out punctuation
     # str vs unicode consideration for translate() behavior
     # python3.6 consideration for using regex here...
+
+    special consideration for \u200e a LTR mark...
+    example and considerations from...
+    https://stackoverflow.com/questions/47861089/correct-length-of-a-string-of-non-english-characters-in-python3
+
+    >> x = 'צוֹר'
+    >> x
+    'צוֹר\u200e' # note the control character escape sequence
+    >> print(len(x))
+    5
+    >> print(len(x.replace('\u200e', ''))
+    4
+
+    : maketrans()
+    The third argument is a string, whose characters will
+    # be mapped to None in the result
+
+    ### Consideration to replace this with
+    >> ''.join(c for c in x if unicodedata.category(c) not in ['Mn', 'Cf'])
+    import consideration needed for unicodedata
     """
 
     import string
     lower_str = raw_data.lower()
+    special_char = '\u200e'
+    punct = string.punctuation.append(special_char)
 
-    # The third argument is a string, whose characters will
-    # be mapped to None in the result
-    new_str = lower_str.translate(str.maketrans('', '', string.punctuation))
+    new_str = lower_str.translate(str.maketrans('', '', punct))
 
     return new_str
